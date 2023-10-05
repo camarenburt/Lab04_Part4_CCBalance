7f247f31d9717621baad4c0c4affc479077f661a
621352 <camarenburt>
2023-10-05 09:23:48 -0400

    eq

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int balance = 5000;
+        double interest = balance *.17;
+        double oneMonth = balance + interest;
+        double interest2 = oneMonth * .17;
+        double twoMonth = oneMonth + interest2;
+        
+
+        System.out.println("After your first month your balance is " + oneMonth +".");
+        System.out.println("After your second month your balance is " + twoMonth +".");
     }
 }
